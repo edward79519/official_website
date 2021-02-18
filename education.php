f36aa4258783a71fe61cd0e652d007ba6cff750f
edward79519 <edward79519@gmail.com>
2021-02-18 17:06:31 +0800

    update education

--- education.php
+++ education.php
@@ -35,7 +35,7 @@
         <div class="inner w1200">
             <div class="card">
                 <div class="card-header">
-                    Taiwan
+                    國內
                 </div>
                 <div class="list-group list-group-flush">
                     <a type="button" class="list-group-item list-group-item-action" href="https://www.re.org.tw/knowledge/default.aspx?cid=201">經濟部能源局 再生能源資訊網 知識中心 焦點專欄</a>
@@ -49,16 +49,18 @@
             </div>
             <div class="card">
                 <div class="card-header">
-                    International
+                    國際
                 </div>
                 <div class="list-group list-group-flush">
-                    <a type="button" class="list-group-item list-group-item-action" href="https://www.re.org.tw/knowledge/default.aspx?cid=201">經濟部能源局 再生能源資訊網 知識中心 焦點專欄</a>
-                    <a type="button" class="list-group-item list-group-item-action" href="https://www.re.org.tw/infographics/default.aspx?cid=224">經濟部能源局 再生能源資訊網 圖文懶人包</a>
-                    <a type="button" class="list-group-item list-group-item-action" href="https://pge.pthg.gov.tw/">綠能專案推動辦公室</a>
-                    <a type="button" class="list-group-item list-group-item-action" href="https://www.mrpv.org.tw/index.aspx">太陽光電單一服務窗口</a>
-                    <a type="button" class="list-group-item list-group-item-action" href="https://www.geipc.tw/">綠能科技產業推動中心</a>
-                    <a type="button" class="list-group-item list-group-item-action" href="https://www.taipower.com.tw/tc/Chart.aspx?mid=194">台電 動態圖表</a>
-                    <a type="button" class="list-group-item list-group-item-action" href="https://www.moeaboe.gov.tw/ECW/populace/content/SubMenu.aspx?menu_id=135">能源局 能源統計</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.energytrend.com.tw/research.html">Energy Trend</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://unfccc.int/process-and-meetings/the-paris-agreement/the-paris-agreement">The Paris Agreement</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://about.bnef.com/blog/">BloombergNEF</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.youtube.com/watch?v=FQ-ClkTZdgg">Planet 2020: Fires And Floods Force Climate Change To The Forefront(YouTube/NBC News)</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.youtube.com/watch?v=b4kZG2SAr1">Climate Change Is a Bigger Disaster Than Coronavirus: Bill Gates(YouTube/Bloomberg Markets and Finance)</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.re.org.tw/knowledge/more.aspx?cid=201&id=3345">解析2019年REN亞太區域 再生能源現況報告</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.re.org.tw/knowledge/more.aspx?cid=201&id=3275">以德國為例，再生能源市場 政策的贏家與輸家</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.re.org.tw/knowledge/more.aspx?cid=201&id=3238">解析亞太區域能源供需展望報告</a>
+
                 </div>
             </div>
         </div>
