f5eace8772ab8db57f921b17dec848a15e42a607
edward79519 <edward79519@gmail.com>
2021-02-18 10:55:41 +0800

    Update Education page

--- education.php
+++ education.php
@@ -9,7 +9,6 @@
 <body>
     <?php include __DIR__.'/include/__page_header.php'; ?>
 
-
     <section id="banner">
         <figure class="banner_pic" style="background-image: url(img/Education/01-ed_banner.jpg);">
             <div class="banner_text">
@@ -25,7 +24,7 @@
         <figure class="banner_pic" style="background-image: url(img/Advantage/05-img.jpg);">
             <div class="banner_text">
                 <h2 class="txt-main">
-                    Taiwan
+                            Link
                 </h2>
                 <h5 class="txt-normal">台灣再生能源/太陽光電相關連結</h5>
             </div>
@@ -35,15 +34,36 @@
     <section id="domestic_link">
         <div class="inner w1200">
             <div class="card">
+                <div class="card-header">
+                    Taiwan
+                </div>
                 <div class="list-group list-group-flush">
                     <a type="button" class="list-group-item list-group-item-action" href="https://www.re.org.tw/knowledge/default.aspx?cid=201">經濟部能源局 再生能源資訊網 知識中心 焦點專欄</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.re.org.tw/infographics/default.aspx?cid=224">經濟部能源局 再生能源資訊網 圖文懶人包</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://pge.pthg.gov.tw/">綠能專案推動辦公室</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.mrpv.org.tw/index.aspx">太陽光電單一服務窗口</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.geipc.tw/">綠能科技產業推動中心</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.taipower.com.tw/tc/Chart.aspx?mid=194">台電 動態圖表</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.moeaboe.gov.tw/ECW/populace/content/SubMenu.aspx?menu_id=135">能源局 能源統計</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    International
+                </div>
+                <div class="list-group list-group-flush">
                     <a type="button" class="list-group-item list-group-item-action" href="https://www.re.org.tw/knowledge/default.aspx?cid=201">經濟部能源局 再生能源資訊網 知識中心 焦點專欄</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.re.org.tw/infographics/default.aspx?cid=224">經濟部能源局 再生能源資訊網 圖文懶人包</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://pge.pthg.gov.tw/">綠能專案推動辦公室</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.mrpv.org.tw/index.aspx">太陽光電單一服務窗口</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.geipc.tw/">綠能科技產業推動中心</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.taipower.com.tw/tc/Chart.aspx?mid=194">台電 動態圖表</a>
+                    <a type="button" class="list-group-item list-group-item-action" href="https://www.moeaboe.gov.tw/ECW/populace/content/SubMenu.aspx?menu_id=135">能源局 能源統計</a>
                 </div>
             </div>
         </div>
     </section>
 
-    </section>
     <?php include __DIR__.'/include/__page_footer.php'; ?>
     <script src="js/main.js"></script>
 </body>
